cded930b967fa9b5979f0d5d5196ab2461029899
Sean Lin <hylin827@gmail.com>
2024-11-17 23:18:30 +0800

    Add sample code for FRQ3 of miterm 1131

---
+++ Midterm1131_frq3.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Midterm1131_frq3 {
+    public static void main(String[] args) {
+        String number="", intLiteral = "-0123456789";
+        
+        Scanner scn = new Scanner(System.in);
+
+        boolean isInt = false;
+        while (!isInt) {
+            System.out.print("請輸入一個整數：");
+            number = scn.next();
+            isInt = true; // assum it is an integer
+            // if it is not an integer, set isInt to false
+            for (int i=0; i<number.length(); i++) {
+                String digit = number.substring(i, i+1);
+                if (intLiteral.indexOf(digit) == -1) {
+                    isInt = false;
+                }
+            }
+        }
+
+        int x = Integer.parseInt(number);
+        double result = 1;        
+        if (x > 0) {
+            for (int i=1; i<=x; i++) {
+                result = result * 10;
+            }
+        } else if (x < 0) {
+            for (int i=1; i<=-x; i++) {
+                result = result / 10;
+            }
+        }
+        System.out.println("10^" + x + " = " + result);
+
+        scn.close();
+    }
+}
