78cb3b7c954e4b25bf24ef35d11a5de723835adb
Sean <hylin827@gmail.com>
2024-12-13 11:58:22 +0800

    add test code for the Card class

--- OO/CardRunner.java
+++ OO/CardRunner.java
@@ -2,7 +2,25 @@ package OO;
 
 public class CardRunner {
     public static void main(String[] args) {
-        Card c1 = new Card(Card.SUITS[0], 0);
-        System.out.println(c1);
+        // // 建立一張牌的範例(Ace, Club)
+        // Card c1 = new Card(Card.SUITS[0], 1);
+        // System.out.println(c1);
+
+        Card[] deck = new Card[52];
+
+        int i=0;
+        for (String suit : Card.SUITS) // for each suit
+        {
+            for (int face=1; face<=13; face++) // for each face
+            {
+                Card c = new Card(suit, face);
+                deck[i] = c;
+                i++;
+            }
+        }
+
+        for (Card card : deck)
+            System.out.println(card);
+
     }
 }
